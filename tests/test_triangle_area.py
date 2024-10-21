39e6cdcb45540aec4e1c9d1ae8c3800764ae85b5
Yehor Yeshchenko <yegor.yeshchenko@gmail.com>
2024-10-21 18:18:25 +0200

    exercises added

---
+++ tests/test_triangle_area.py
@@ -0,0 +1,14 @@
+from test_functions import triangle_area
+
+
+class TestTrianglePositive:
+    def test_triangle_1(self):
+        assert round(triangle_area(1, 1, 1), 3) == 0.433
+
+    def test_triangle_1_2(self):
+        assert round(triangle_area(1, 1, 1), 3) == 0.433
+
+
+def test_triangle_2():
+    assert round(triangle_area(2, 2, 2), 3) == 1.732
+
